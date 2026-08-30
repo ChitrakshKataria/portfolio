a19ba187dc3bdc8850f9c7d4e6392b8be2046c45
ChitrakshKataria <chitrakshkatariv3@gmail.com>
2026-08-30 21:05:43 +0200

    Added a custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function notFound() {
+    return (
+        <div className="text-center">
+            <h1 className="text-3xl">404 - Notfound</h1>
+            <p>We looked everywhere even under the divs</p>
+            <p className="fixed bottom-4 right-4 text-[var(--background)] mb-17 mr-50">You found the secret text but not the page unfortunately</p>
+            </div>
+    );
+}
